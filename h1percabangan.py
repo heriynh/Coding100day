b7099b39a86d81b9398e401bc8644084dc135928
heriynh <heriyynh@gmail.com>
2021-12-18 16:28:19 +0700

    Coding h1

---
+++ h1percabangan.py
@@ -0,0 +1,11 @@
+
+angka = 20
+
+if angka <=5 :
+    print("Bayi")
+elif angka >=6 and angka <=12:
+    print("anak-anak")
+elif angka >=13 and angka <=18:
+    print("Remaja")
+else :
+    print("Dewasa")
