15cde7108e918781c12465084b19aa356c7b105d
heriynh <91871156+heriynh@users.noreply.github.com>
2021-12-21 16:55:31 +0700

    h9 ngoding

---
+++ h9array.py
@@ -0,0 +1,15 @@
+
+
+hari = "rabu"
+a = "baju putuh abu-abu"
+b = "baju bati"
+c = "baju pramuka"
+
+if hari=="senin" or hari=="selasa":
+    print(a)
+elif hari=="rabu" or hari=="kamis":
+    print(b)
+elif hari=="jumat" or hari=="sabtu":
+    print(c)
+else:
+    print("bukan hari sekolah")
