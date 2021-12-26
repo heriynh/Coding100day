c0c4295ff1a7888bdaac2e14381f56d341c15725
heriynh <91871156+heriynh@users.noreply.github.com>
2021-12-26 22:13:30 +0700

    h14 ngoding belajar aritmatika

---
+++ h14aritmatika.py
@@ -0,0 +1,15 @@
+
+
+bil1 = int(input("masukan bilangan pertama :"))
+bil2 = int(input("masukan bilangan kedua :"))
+
+
+jumlah = bil1+bil2
+kurang = bil1-bil2
+bagi = bil1/bil2
+kali = bil1*bil2
+
+print("hasil jumlah =", jumlah)
+print("hasil kurang =", kurang)
+print("hasil bagi =", bagi)
+print("hasil kali =", kali)
