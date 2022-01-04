8e433bb4cd453f85ef11a725cfc4fcd4cc95cd2c
heriynh <91871156+heriynh@users.noreply.github.com>
2022-01-04 19:28:47 +0800

    Day 24 ngoding, belajar membuat program sederhana menghitung keliling lingkaran

---
+++ day24.py
@@ -0,0 +1,11 @@
+
+
+#menghitung luas lingkaran 
+v = 3.14
+
+a = int(input("Masukkan jari jari lingkaran : "))
+
+keliling = 2 * v *a
+
+print ("jika jari jari lingkaran =",a)
+print("maka keliling lingkaran =",keliling)
