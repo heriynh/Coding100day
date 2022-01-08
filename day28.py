43651c87b043238130f9ef5e221a1167348186f1
heriynh <91871156+heriynh@users.noreply.github.com>
2022-01-08 19:47:26 +0800

    day 28 ngoding belajar membuat program menghitung luas segitiga

---
+++ day28.py
@@ -0,0 +1,10 @@
+
+
+#program sederhana menghitung luas segitiga
+
+alas = int(input("Masukkan alas segitiga :"))
+tinggi = int(input("Masukkan tinggi segitiga :"))
+
+rumus = 0.5 * alas * tinggi
+a = rumus
+print("Luas segitiga :", a)
