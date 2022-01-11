28855e89c056e22b56c7758862058eaca69794ab
heriynh <91871156+heriynh@users.noreply.github.com>
2022-01-11 19:06:57 +0800

    Day 31 belajar ngoding

---
+++ day31.py
@@ -0,0 +1,11 @@
+
+
+
+#program sederhana python
+print("-Jika kamu memasukkan bilangan genap maka tampil bulan dan jika bilangan ganjil maka tampil bintang")
+
+angka = int(input("Masukkan Bilangan :"))
+if angka %2 == 0:
+	print("Bulan")
+else:
+	print("Bintang")
