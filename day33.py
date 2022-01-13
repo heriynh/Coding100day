922943499743b76bca085a32db2c2c722e769348
heriynh <91871156+heriynh@users.noreply.github.com>
2022-01-13 18:56:59 +0800

    Day 33 belajar ngoding

---
+++ day33.py
@@ -0,0 +1,11 @@
+
+
+# belajar program python
+print("-----Login-----")
+user = input("Masukkan username :")
+pas = input("Masukkan password :")
+
+if user == "heri" and pas== "123" :
+	print("Selamat anda berhasil login")
+else:
+	print("Username dan password salah")
