79b36981fbde37c44b329eb31fc93db1827f3f39
heriynh <91871156+heriynh@users.noreply.github.com>
2022-01-14 18:48:03 +0800

    Day 34 belajar ngoding

---
+++ day34.py
@@ -0,0 +1,15 @@
+
+
+
+#program python
+
+print("Peraturan desa")
+
+kk = 5
+
+anak = int(input("Masukkan jumlah anak :"))
+if anak > 2:
+	print(anak,"Anak untuk 3 KK dari",str(kk),"Kk")
+else :
+	print(anak,"Anak untuk 2 KK dari",str(kk),"Kk")
+	
