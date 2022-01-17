92859df03badc5b65f2ea7d88ddb00c0983c587a
heriynh <91871156+heriynh@users.noreply.github.com>
2022-01-17 18:49:52 +0800

    Day 37 belajar ngoding

---
+++ day37.py
@@ -0,0 +1,11 @@
+
+
+
+
+# program sederhana python
+
+barang = ("Handphone","Leptop","Ram","Power suplay","Hardisk","Monitor","Keyboard","Mouse","Lan card","VGA","Heatsink","Processor")
+
+
+for a in (barang):
+	print("Barang elektronik",a)
