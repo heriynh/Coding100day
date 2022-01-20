b2491494006d7665ec372b3b37a5ccfaa7734b70
heriynh <91871156+heriynh@users.noreply.github.com>
2022-01-20 09:23:05 +0800

    Day 39 coding Rpl

---
+++ day39.py
@@ -0,0 +1,11 @@
+
+
+#proram python sederhana input tipe string
+
+nama = input("Masukkan nama anda :")
+nim = input("Masukkan nim :")
+kelas = input("Masukkan kelas :")
+
+print("Nama  :",nama)
+print("Nim :",nim)
+print("Kelas :"kelas)
