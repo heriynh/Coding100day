fd3ae56f0b0eb701e058ce371f0e2523b3956681
heriynh <91871156+heriynh@users.noreply.github.com>
2022-01-24 17:23:59 +0800

    Day 44 belajar ngoding bareng RPL Lab

---
+++ day44.py
@@ -0,0 +1,13 @@
+
+
+#program python sederhana menghitung luas segitiga
+
+phi = 0.5
+
+alas = int(input("Masukkan alas :"))
+tinggi = int(input("Masukkan tinggi :"))
+rumus = phi *alas*tinggi
+
+print("Jika alas =",str(alas))
+print("dan tinggi =",str(tinggi))
+print("Maka luas segitiga =",str(rumus))
