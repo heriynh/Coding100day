049c054b437d416c4b48a4273ee17fba7ba49d2c
heriynh <91871156+heriynh@users.noreply.github.com>
2022-01-26 18:04:31 +0800

    Day 46 belajar ngoding bareng RPL Lab

---
+++ day46.py
@@ -0,0 +1,12 @@
+
+#program python sederhana menghitung luas lingkaran
+
+
+print("-------Mentukan luas lingkaran----------")
+
+r = int(input("Masukkan jari-jari lingkaran :"))
+phi = 3.14
+
+luas = phi*(r*r)
+print("Jika jari-jari lingkaran =",str(r))
+print("Maka luas lingkaran =",str(luas))
