528fc334ec7efb311e27c74925c1975faf3d0377
heriynh <91871156+heriynh@users.noreply.github.com>
2022-01-29 18:44:51 +0800

    Day 49 belajar ngoding bareng RPL Lab

---
+++ day49.py
@@ -0,0 +1,20 @@
+
+
+#program python aritmatika menggunakan input
+
+bil1 = int(input("Masukkan angka pertama :"))
+bil2 = int(input("Masukkan angka kedua :"))
+
+#aritmatika
+
+kali = bil1*bil2
+bagi = bil1/bil2
+tambah = bil1+bil2
+kurang = bil1-bil2
+
+#menampilkan
+
+print("Hasil kali bilangan pertama dan bilangan kedua",kali)
+print("Hasil bagi bilangan pertama dan bilangan kedua",bagi)
+print("Hasil tambah bilangan pertama dan  bilangan kedua",tambah)
+print("Hasil kurang bilangan pertama dan bilangan kedua",kurang)
