9226d39fd50ecbf1ccc946fffd0692da562d6cbe
heriynh <91871156+heriynh@users.noreply.github.com>
2022-01-31 18:28:43 +0800

    Day 51 belajar ngoding bareng RPL Lab

---
+++ day51.py
@@ -0,0 +1,14 @@
+
+
+#program python sederhana
+
+
+#dictionary
+
+mahasiswa ={
+	"nama" : "Heri",
+	"nim" : "D0218014",
+	"Kelas" : "Informatika B 2018",
+	"Alamat" : "Sendana"
+}
+print(mahasiswa)
