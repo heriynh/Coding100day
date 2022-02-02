8cdfb25ec35c5f2a6584b5bc34165d84a559b27b
heriynh <91871156+heriynh@users.noreply.github.com>
2022-02-02 18:58:47 +0800

    Day 53 belajar ngoding bareng RPL Lab

---
+++ day53.py
@@ -0,0 +1,15 @@
+
+
+#program python sederhana form login
+
+
+user = input("Masukkan username :")
+pas = input("Masukkan password :")
+
+#equals
+
+
+if user=="admin" and pas=="123":
+	print("Selamat anda berhasil login")
+else:
+	print("Username atau password salah")
