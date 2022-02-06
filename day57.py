4a14605dcd2d75ede3002614a38bdef8a96646be
heriynh <91871156+heriynh@users.noreply.github.com>
2022-02-06 19:07:27 +0700

    Day 57 belajar ngoding bareng RPL Lab

---
+++ day57.py
@@ -0,0 +1,11 @@
+
+
+#program python sederhana day 57 luas segitiga
+
+
+alas = float(input("masukkan alas :"))
+tinggi = float(input("Masukka tinggi :"))
+
+rumus = 0.5 * alas * tinggi
+
+print("Luas segitiga =",rumus)
