262baebb0e8e35eb31ec7da5ca58ed69c247ba18
heriynh <91871156+heriynh@users.noreply.github.com>
2022-02-10 17:39:50 +0700

    Day 61 belajar ngoding bareng RPL Lab

---
+++ day61.py
@@ -0,0 +1,13 @@
+
+
+#program python sederhana menentukan nilai  min dan max
+
+#deklarasi variabel
+a = [20,40,60,80,100]
+
+#output
+
+print(a)
+print("---------------------")
+print("Nilai terkecil :",min(a))
+print("Nilai terbesar :",max(a))
