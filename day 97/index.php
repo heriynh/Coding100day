e4b20feff0a1db18fd78811022b340fec15e7c06
heriynh <91871156+heriynh@users.noreply.github.com>
2022-03-18 19:10:24 +0700

    day 97 belajar ngoding bareng Rpl Lab

---
+++ day 97/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Dashboard Admin Apk</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <!-- Navbar Brand-->
+        <a class="navbar-brand ps-3" href="index.html">Internet of Things</a>
+    </nav>
+    <div id="layoutSidenav_content">
+        <main>
+            <div class="container-fluid px-4" style="margin-top: 100px;">
+                <h3 class="mt-4" style="text-align: center;">Mendeteksi Kebocoran Pipa Tanaman Hidroponik</h3>
+                <h3 class="mt-0" style="text-align: center;">Secara Realtime</h3>
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="row">
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-info text-white mb-4">
+                                    <div class="card-body">Lihat kecepatan air</div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-info text-white mb-4">
+                                    <div class="card-body">Kondisi pipa</div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-info text-white mb-4">
+                                    <div class="card-body">Jumlah air</div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- <table class="table text-center border-1">
+                            <tr style="background-color: aquamarine; color: black;">
+                                <th>Kecepatan air</th>
+                                <th>Kondisi pipa</th>
+                                <th>Jumlah air</th>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>Sangat baik</td>
+                                <td>100 Liter</td>
+                            </tr>
+                        </table> -->
+                    </div>
+                </div>
+
+            </div>
+        </main>
+        <footer class="py-4 bg-dark fixed-bottom mt-auto">
+            <div class="container-fluid px-4">
+                <div class="d-flex align-items-center justify-content-between small">
+                    <div class="text-muted">Copyright &copy; Your Website 2021</div>
+                    <div>
+                        <a href="#">Privacy Policy</a>
+                        &middot;
+                        <a href="#">Terms &amp; Conditions</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+    <script src="js/datatables-simple-demo.js"></script>
+</body>
+
+</html>
